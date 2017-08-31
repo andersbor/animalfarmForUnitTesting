eaf9eec1552a9c2d8c8fa1f06f62bd01fc24e1ef
andersbor <anbo@easj.dk>
2017-08-31 12:36:33 +0200

    first commit

---
+++ ClassLibrary1/Cow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    /// <summary>
+    /// Describes a cow 
+    /// Invariant: weight >= 74
+    /// </summary>
+    public class Cow
+    {
+        private int _weight;
+
+        // TODO Cow constructor with parameters
+        // HACK something should be changed
+
+        /// <summary>
+        /// The weight of a cow
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public int Weight
+        {
+            get { return _weight; }
+            set
+            {
+                if (value < 74)
+                    throw new ArgumentOutOfRangeException("weight", value, "weight skal være min 74");
+                _weight = value;
+            }
+        }
+    }
+}
---
+++ FarmTest/CowTEst.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FarmTest
+{
+    [TestClass]
+    public class CowTest
+    {
+        private Cow ko;
+        [TestInitialize]
+        public void SetUpCow()
+        {
+            ko = new Cow();
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ko.Weight = 80;
+            try
+            {
+                ko.Weight = 73;
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException)
+            { }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestWeight()
+        {
+            ko.Weight = 73;
+            Assert.AreEqual(73, ko.Weight);
+        }
+    }
+}
